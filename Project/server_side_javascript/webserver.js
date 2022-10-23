1dedf80aff7179b8ece2f34a5bf4b3c966d83740
plitche <kysu728@naver.com>
2022-10-23 18:35:35 +0900

    6. 간단한 웹앱 만들기 - 정리

--- Project/server_side_javascript/webserver.js
+++ Project/server_side_javascript/webserver.js
@@ -3,12 +3,12 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { // 서버를 만든다.
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, hostname, () => { // 만든 서버를 listen 시킨다.
   console.log(`Server running at http://${hostname}:${port}/`);
 });
