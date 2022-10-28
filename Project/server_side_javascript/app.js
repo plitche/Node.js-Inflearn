309a31c052fe8c773fc319362b2dc8fe153b1433
plitche <kysu728@naver.com>
2022-10-28 22:58:27 +0900

    18. Express - Express, 정적 파일을 서비스 하는 법

--- Project/server_side_javascript/app.js
+++ Project/server_side_javascript/app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public')) // 정적 리소스를 서비스하기위한 디렉토리 설정
 
 app.get('/', function(req, res) {
     res.send('Hello home page');
 });
+
+app.get('/staticTest', function(req, res) {
+    res.send('Hello Router, <div></div>')
+})
 
 app.get('/login', function(req, res) {
     res.send('Login please');
@@ -12,3 +17,4 @@ app.get('/login', function(req, res) {
 app.listen(3000, function() {
     console.log('Connted 3000 port!');
 })
+
