a42ff727120d44df6689bf4829c7f5f3c901a71e
plitche <kysu728@naver.com>
2022-10-30 00:02:19 +0900

    21. Express 템플릿 엔진 - Express, 템플릿 엔진 사용법

--- Project/server_side_javascript/app.js
+++ Project/server_side_javascript/app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+app.set('views', './views')
+app.set('view engine', 'pug');
 app.use(express.static('public')) // 정적 리소스를 서비스하기위한 디렉토리 설정
+
+app.get('/template', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+  });
 
 app.get('/', function(req, res) {
     res.send('Hello home page');
