a22d4d3ff827706d2492f8d4ee75296fde0bdb89
plitche <kysu728@naver.com>
2024-08-16 23:07:13 +0900

    express로 html 서빙하기

--- Zerocho/Project/section 5/app.js
+++ Zerocho/Project/section 5/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000); // 서버에 속성를 심는다. (port속성에 3000부여)
 
 app.get('/', (req, res) => {
-    res.send('hello express')
+    res.sendFile(path.join(__dirname, 'index.html')) // __dirname 현재 파일의 절대 경로
 })
 
 app.post('/', (req, res) => {
