e777e877d8f17c7a1883ac803faa0b21ceb99b3b
plitche <kysu728@naver.com>
2024-08-19 21:26:38 +0900

    static 미들웨어

--- Zerocho/Project/section 5-2/app.js
+++ Zerocho/Project/section 5-2/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const session = require('express-session')
+const multer = require('multer')
+
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+// 미들 웨어들 간에 순서도 중요하다.
 app.use(morgan('dev')); // 요청과 응답을 기록하는 라우터 'dev', 'combiend'
+// 정적파일 읽을 때(요청경로, express.static(실제 경로))
+// localhost:3000/zerocho.html -> learn-express/public/zerocho.html
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json()); // bodyParser 대체, client가 json데이터를 보냈을때 parsing 해서 body에 넣어준다.
 app.use(express.urlencoded({ extended: true })); // form submit 데이터를 parsing 해준다.
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session())
+app.use(multer().array())
 
 app.get('/', (req, res, next) => {
     console.log('GET / 요청에서만 실행됩니다.');
