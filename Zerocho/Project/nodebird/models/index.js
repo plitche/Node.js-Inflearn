85ace33ed0339e99f9ffc5f693c7249796a7eea8
plitche <kysu728@naver.com>
2024-09-02 00:11:12 +0900

    데이터베이스 구조 갖추기

--- Zerocho/Project/nodebird/models/index.js
+++ Zerocho/Project/nodebird/models/index.js
@@ -1,43 +1,27 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const config  = require('../config/config.json')[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const User = require('./user');
+const Post = require('./post');
+const HashTag = require('./hashtag');
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const db        = {};
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+)
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.User = User;
+db.Post = Post;
+db.HashTag = HashTag;
+
+User.init(sequelize);
+Post.init(sequelize);
+HashTag.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+HashTag.associate(db);
 
 module.exports = db;
